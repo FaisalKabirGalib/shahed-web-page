e50ed6db1c9e90e56e9b2b3fbea22a0b41ba6832
Faisal Kabir Galib <faisalkabirru@gmail.com>
2023-04-20 02:42:55 +0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div>
+        <a href="#" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="#" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Xgarden.com</h1>
+      <p>This is Under construction. </p>
+
+    </div>
+  )
+}
+
+export default App
